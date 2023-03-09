3ddaa5da4a147a5e012a57c9e30c6ca12851e680
dave-macleod <127327064+dave-macleod@users.noreply.github.com>
2023-03-09 10:25:45 +0000

    Update USBMultiMediaController.ino

--- USBMultiMediaController.ino
+++ USBMultiMediaController.ino
@@ -3,12 +3,9 @@ Name
   USB Multimedia Controller
 Purpose
   Using Arduino ATMega32u4 5v, aka Pro Micro, take input from rotary encoder with switch and mimic a USB HID Keyboard to send the commands to the connected computer (PC or Mac).
-    - Short press/release knob 1 to play/resume
-    - Long press/release knob 1 to skip to next track
-    - Rotate knob 1 to adjust volume up and down
-    - Short press/release knob 2 to do open OldReader item (b)
-    - Long press/release knob 1 to do star OpenReader post (f)
-    - Rotate knob 2 to do navigate OldReader next/previous (n/p)
+    - Short press/release knob to play/pause
+    - Long press/release knob to skip to next track
+    - Rotate knob to adjust volume up and down
 
 Author
   Dave MacLeod damacleod@gmail.com
@@ -31,7 +28,7 @@ const int encSwitch = 5; // pause/play button
 // variables
 long encPrevRotationValue  = -999; 
 long encRotationValue = -999;
-long encScale = 5;
+long encScale = 3.3;   // You will need to experiment with this value - I wanted a single click on my encoder to match a single increment of volume on my Mac Mini (M1)
 int encSwitchState = 0;
 bool encSwitchPressed = false;
 unsigned long encSwitchPressTime = 0;
